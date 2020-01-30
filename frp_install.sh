a95a8e3f250c73d5822cfa80551b32cb3b50cee9
root <cjjmpgjjl.113@gmail.com>
2020-01-30 00:52:58 -0500

    Give frps binary access to well-known ports(smbd,NetBIOS,etc).

--- frp_install.sh
+++ frp_install.sh
@@ -1,7 +1,9 @@
+#Get latest release tag
 RELEASETAG=$(git ls-remote --tags https://github.com/fatedier/frp | sort -t '/' -k 2 -V | sed -e '/\^/d')
 export FRP_VERSION=${RELEASETAG:0-6}
 echo "The latest frp release is ${FRP_VERSION}"
 mkdir -p /etc/frp
+rm -rf /tmp/frp_${FRP_VERSION}_linux_amd64*
 cd /tmp
  echo "Downloading tar package from Github......"
  wget -q "https://github.com/fatedier/frp/releases/download/v${FRP_VERSION}/frp_${FRP_VERSION}_linux_amd64.tar.gz"
@@ -11,6 +13,7 @@ cd /tmp
  echo "Installing binary files......"
  cp ./frp_${FRP_VERSION}_linux_amd64/frps /usr/bin && chmod +x /usr/bin/frps
  cp ./frp_${FRP_VERSION}_linux_amd64/frpc /usr/bin && chmod +x /usr/bin/frpc
+ setcap cap_net_bind_service=ep /usr/bin/frps #Give frps binary access to well-known ports(smbd,NetBIOS,etc).
  echo "Installing configuration files......"
  test -e /etc/frp/frpc.ini  && echo "Configuration files exist." || echo "Configuration files do not exist. Copy templetes." && cp ./frp_${FRP_VERSION}_linux_amd64/frp*.ini /etc/frp
  echo "Configuration file is installed to /etc/frp ."
@@ -18,4 +21,6 @@ cd /tmp
  echo "Install complete."
  echo "See https://github.com/fatedier/frp/blob/master/README.md for configuration."
  echo "Use \"systemctl start frps\" or \"systemctl start frpc\" to start frpc/frps service."
+ #systemctl enable frps
+ #systemctl enable frpc
  rm -rf /tmp/frp_${FRP_VERSION}_linux_amd64*
